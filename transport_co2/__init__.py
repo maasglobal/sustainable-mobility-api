f1f97ab1bfcdbc2fa9fc55435eda572f46af9c32
Markus Shepherd <markus.schepke@maas.global>
2019-11-15 13:55:32 +0200

    added transport_co2/__version__.py

--- transport_co2/__init__.py
+++ transport_co2/__init__.py
@@ -2,5 +2,6 @@
 
 """Library to calculate CO2 (equivalent) emissions for a given transport trip."""
 
+from .__version__ import VERSION, __version__
 from .model import Mode
 from .estimator import estimate_co2
---
+++ transport_co2/__version__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""Version."""
+
+VERSION = (0, 1, 1)
+__version__ = ".".join(map(str, VERSION))
