7dc44e960e759c9c3f19105a7d17d3fce2fc6e09
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-27 14:49:14 +0200

    Initial working endpoint

--- api/app.py
+++ api/app.py
@@ -2,8 +2,15 @@ import connexion
 from transport_co2 import estimate_co2
 
 
-def get_co2_estimate():
-    return "hello world"
+def get_co2_estimate(transport_mode=None, distance_km=None):
+
+    if transport_mode != None and distance_km != None:
+        co2_estimate = estimate_co2(
+            mode=transport_mode, distance_in_km=distance_km)
+
+        return {
+            "co2_estimate": co2_estimate,
+        }
 
 
 app = connexion.FlaskApp(__name__)
