cf26088bd0ac365027092614860c107e655e8eca
Ruslan Gainutdinov <ruslan.gainutdinov@maas.global>
2020-01-31 16:40:03 +0200

    Code review fixes

--- api/api.py
+++ api/api.py
@@ -1,7 +1,7 @@
 import connexion
 import os
 from haversine import haversine
-from transport_co2 import estimate_co2
+from transport_co2 import estimate_co2, Mode
 
 def verify_origin_destination_coordinates_were_provided(origin_lat, origin_lon, destination_lat, destination_lon):
     # Ensure all origin/destination coordinates were provided
@@ -28,19 +28,25 @@ def get_co2_estimate(transport_mode=None, distance_km=None, vehicle_occupancy=No
         return {
             "error": "Not enough information was provided to calculate CO2 estimate."
         }
+    
+    if isinstance(transport_mode, str):
+        transport_mode = Mode[transport_mode.upper()]
+
+    if vehicle_occupancy == None:
+        vehicle_occupancy = transport_mode.average_occupancy
 
     co2_estimate = estimate_co2(mode=transport_mode, distance_in_km=distance_km, occupancy=vehicle_occupancy)
 
     return_data = {
         "transport_mode": str(transport_mode),
         "vehicle_occupancy": vehicle_occupancy,
+        "distance_km": distance_km,
         "co2_estimate": co2_estimate
     }
 
     return return_data
 
-if os.environ.get("AWS_EXECUTION_ENV") == None:
+if __name__ == "__main__":
     app = connexion.FlaskApp(__name__)
     app.add_api("specification.json")
     app.run(port=8080)
-
--- api/handler.py
+++ api/handler.py
@@ -2,6 +2,7 @@ import json
 import os
 from api import get_co2_estimate
 from transport_co2 import Mode
+import logging
 
 def strtofloat(val):
     if isinstance(val, str):
@@ -11,48 +12,7 @@ def strtofloat(val):
 def post(event, context):
     try:
         vars = event.get("queryStringParameters", dict())
-        if vars != None:
-            print(f'estimate-co2 sourceIp={event["requestContext"]["identity"]["sourceIp"]} userAgent={event["requestContext"]["identity"]["userAgent"]}, input={vars}')
-            
-            transport_mode = vars.get("transport_mode")
-            if isinstance(transport_mode, str):
-                transport_mode = Mode[transport_mode.upper()]
-
-            distance_km = strtofloat(vars.get("distance_km"))
-            vehicle_occupancy = strtofloat(vars.get("vehicle_occupancy"))
-            origin_lat = strtofloat(vars.get("origin_lat"))
-            origin_lon = strtofloat(vars.get("origin_lon"))
-            destination_lat = strtofloat(vars.get("destination_lat"))
-            destination_lon = strtofloat(vars.get("destination_lon"))
-
-            estimate = get_co2_estimate(
-                transport_mode = transport_mode,
-                distance_km = distance_km,
-                vehicle_occupancy = vehicle_occupancy, 
-                origin_lat = origin_lat, 
-                origin_lon = origin_lon,
-                destination_lat = destination_lat, 
-                destination_lon = destination_lon
-            )
-
-            if estimate.get("error") == None:
-                response = {
-                    "statusCode": 200,
-                    "body": json.dumps({
-                        "input": vars,
-                        "result": estimate
-                    })
-                }
-                return response
-            else:
-                response = {
-                    "statusCode": 400,
-                    "body": json.dumps({
-                        "error:": estimate.get("error")
-                    })
-                }
-                return response
-        else:
+        if not vars:
             response = {
                 "statusCode": 400,
                 "body": json.dumps({
@@ -60,8 +20,51 @@ def post(event, context):
                 })
             }
             return response
+        
+        logging.info(f'estimate-co2 sourceIp={event["requestContext"]["identity"]["sourceIp"]} userAgent={event["requestContext"]["identity"]["userAgent"]}, input={vars}')
+        
+        transport_mode = vars.get("transport_mode")
+        if isinstance(transport_mode, str):
+            transport_mode = Mode[transport_mode.upper()]
+
+        distance_km = strtofloat(vars.get("distance_km"))
+        vehicle_occupancy = strtofloat(vars.get("vehicle_occupancy"))
+        origin_lat = strtofloat(vars.get("origin_lat"))
+        origin_lon = strtofloat(vars.get("origin_lon"))
+        destination_lat = strtofloat(vars.get("destination_lat"))
+        destination_lon = strtofloat(vars.get("destination_lon"))
+
+        estimate = get_co2_estimate(
+            transport_mode = transport_mode,
+            distance_km = distance_km,
+            vehicle_occupancy = vehicle_occupancy, 
+            origin_lat = origin_lat, 
+            origin_lon = origin_lon,
+            destination_lat = destination_lat, 
+            destination_lon = destination_lon
+        )
+
+        if estimate.get("error"):
+            response = {
+                "statusCode": 400,
+                "body": json.dumps({
+                    "error:": estimate.get("error")
+                })
+            }
+            return response
+            
+        response = {
+            "statusCode": 200,
+            "body": json.dumps({
+                "input": vars,
+                "result": estimate
+            })
+        }
+        return response
+            
+            
     except Exception as err:
-        print(f"Got exception processing request: {type(err)}: {err}")
+        logging.exception(f"Got exception processing request: {type(err)}: {err}")
         return {
             "statusCode": 500,
             "body": json.dumps({
