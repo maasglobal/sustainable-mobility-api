7545546ba5c91f221c93e6494b5f0eb9222c8d7e
Markus Shepherd <markus.schepke@maas.global>
2020-02-05 12:37:31 +0200

    linted handler.py

--- api/handler.py
+++ api/handler.py
@@ -1,39 +1,58 @@
+# -*- coding: utf-8 -*-
+
+"""Serverless handler."""
+
 import json
-from api import get_co2_estimate
-from transport_co2 import Mode
 import logging
+
+from transport_co2 import Mode
+
+from api import get_co2_estimate
+
+LOGGER = logging.getLogger(__name__)
 
 
 def strtofloat(val):
+    """Parse str to float."""
     if isinstance(val, str):
         val = float(val)
     return val
 
 
+# pylint: disable=unused-argument
 def post(event, context):
+    """Serverless handler."""
+
     try:
-        vars = event.get("queryStringParameters", dict())
-        if not vars:
+        params = event.get("queryStringParameters", dict())
+        if not params:
             response = {
                 "statusCode": 400,
-                "body": json.dumps({"error:": "No parameters provided",}),
+                "body": json.dumps({"error": "No parameters provided"}),
             }
             return response
 
-        logging.info(
-            f'estimate-co2 sourceIp={event["requestContext"]["identity"]["sourceIp"] if event.get("requestContext") else "?"} userAgent={event["requestContext"]["identity"]["userAgent"] if event.get("requestContext") else "?"}, input={vars}'
+        LOGGER.info(
+            "estimate-co2 sourceIp=%s userAgent=%s, input=%s",
+            event["requestContext"]["identity"]["sourceIp"]
+            if event.get("requestContext")
+            else "?",
+            event["requestContext"]["identity"]["userAgent"]
+            if event.get("requestContext")
+            else "?",
+            params,
         )
 
-        transport_mode = vars.get("transport_mode")
+        transport_mode = params.get("transport_mode")
         if isinstance(transport_mode, str):
             transport_mode = Mode[transport_mode.upper()]
 
-        distance_km = strtofloat(vars.get("distance_km"))
-        vehicle_occupancy = strtofloat(vars.get("vehicle_occupancy"))
-        origin_lat = strtofloat(vars.get("origin_lat"))
-        origin_lon = strtofloat(vars.get("origin_lon"))
-        destination_lat = strtofloat(vars.get("destination_lat"))
-        destination_lon = strtofloat(vars.get("destination_lon"))
+        distance_km = strtofloat(params.get("distance_km"))
+        vehicle_occupancy = strtofloat(params.get("vehicle_occupancy"))
+        origin_lat = strtofloat(params.get("origin_lat"))
+        origin_lon = strtofloat(params.get("origin_lon"))
+        destination_lat = strtofloat(params.get("destination_lat"))
+        destination_lon = strtofloat(params.get("destination_lon"))
 
         estimate = get_co2_estimate(
             transport_mode=transport_mode,
@@ -54,13 +73,13 @@ def post(event, context):
 
         response = {
             "statusCode": 200,
-            "body": json.dumps({"input": vars, "result": estimate}),
+            "body": json.dumps({"input": params, "result": estimate}),
         }
         return response
 
-    except Exception as err:
-        logging.exception(f"Got exception processing request: {type(err)}: {err}")
+    except Exception:
+        LOGGER.exception("Got exception processing request: %r", event)
         return {
             "statusCode": 500,
-            "body": json.dumps({"error:": "Internal server error processing request"}),
+            "body": json.dumps({"error": "Internal server error processing request"}),
         }
