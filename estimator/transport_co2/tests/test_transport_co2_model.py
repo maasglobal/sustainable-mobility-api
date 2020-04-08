d6a960880d5daf177cbc1e1849088de76c3b1789
Brylie Christopher Oxley <brylie@amble.fi>
2020-04-08 14:42:07 +0300

    Use approx; lint

--- estimator/transport_co2/tests/test_transport_co2_model.py
+++ estimator/transport_co2/tests/test_transport_co2_model.py
@@ -1,62 +1,63 @@
+from pytest import approx
 from transport_co2.model import Mode
 
 # Only distance
 def test_estimate_co2_bus():
-    co2_estimate = Mode["BUS"].estimate_co2(distance_in_km=10)
+    co2_estimate = Mode.BUS.estimate_co2(distance_in_km=10)
 
-    assert co2_estimate == 679.5275590551181
+    assert co2_estimate == approx(679.5275590551181)
 
 
 def test_estimate_co2_light_rail():
-    co2_estimate = Mode["LIGHT_RAIL"].estimate_co2(distance_in_km=10)
+    co2_estimate = Mode.LIGHT_RAIL.estimate_co2(distance_in_km=10)
 
-    assert co2_estimate == 140
+    assert co2_estimate == approx(140)
 
 
 def test_estimate_co2_small_car():
-    co2_estimate = Mode["SMALL_CAR"].estimate_co2(distance_in_km=10)
+    co2_estimate = Mode.SMALL_CAR.estimate_co2(distance_in_km=10)
 
-    assert co2_estimate == 1120
+    assert co2_estimate == approx(1120)
 
 
 def test_estimate_co2_large_car():
-    co2_estimate = Mode["LARGE_CAR"].estimate_co2(distance_in_km=10)
+    co2_estimate = Mode.LARGE_CAR.estimate_co2(distance_in_km=10)
 
-    assert co2_estimate == 1466.6666666666667
+    assert co2_estimate == approx(1466.6666666666667)
 
 
 def test_estimate_co2_scooter():
-    co2_estimate = Mode["SCOOTER"].estimate_co2(distance_in_km=10)
+    co2_estimate = Mode.SCOOTER.estimate_co2(distance_in_km=10)
 
-    assert co2_estimate == 720
+    assert co2_estimate == approx(720)
 
 
 # Distance and occupancy
 def test_estimate_co2_bus_with_occupancy():
-    co2_estimate = Mode["BUS"].estimate_co2(distance_in_km=10, occupancy=10)
+    co2_estimate = Mode.BUS.estimate_co2(distance_in_km=10, occupancy=10)
 
-    assert co2_estimate == 863
+    assert co2_estimate == approx(863)
 
 
 def test_estimate_co2_light_rail_with_occupancy():
-    co2_estimate = Mode["LIGHT_RAIL"].estimate_co2(distance_in_km=10, occupancy=100)
+    co2_estimate = Mode.LIGHT_RAIL.estimate_co2(distance_in_km=10, occupancy=100)
 
-    assert co2_estimate == 218.4
+    assert co2_estimate == approx(218.4)
 
 
 def test_estimate_co2_small_car_with_occupancy():
-    co2_estimate = Mode["SMALL_CAR"].estimate_co2(distance_in_km=10, occupancy=2)
+    co2_estimate = Mode.SMALL_CAR.estimate_co2(distance_in_km=10, occupancy=2)
 
-    assert co2_estimate == 840.0
+    assert co2_estimate == approx(840.0)
 
 
 def test_estimate_co2_large_car_with_occupancy():
-    co2_estimate = Mode["LARGE_CAR"].estimate_co2(distance_in_km=10, occupancy=3)
+    co2_estimate = Mode.LARGE_CAR.estimate_co2(distance_in_km=10, occupancy=3)
 
-    assert co2_estimate == 733.3333333333334
+    assert co2_estimate == approx(733.3333333333334)
 
 
 def test_estimate_co2_scooter_with_occupancy():
-    co2_estimate = Mode["SCOOTER"].estimate_co2(distance_in_km=10, occupancy=2)
+    co2_estimate = Mode.SCOOTER.estimate_co2(distance_in_km=10, occupancy=2)
 
-    assert co2_estimate == 432.0
+    assert co2_estimate == approx(432.0)
