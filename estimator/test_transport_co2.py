fcb9d8d330bd38e874dc72e3aa7994395098af50
Brylie Christopher Oxley <brylie@amble.fi>
2020-04-08 12:34:32 +0300

    Add tests with occupancy

--- estimator/test_transport_co2.py
+++ estimator/test_transport_co2.py
@@ -1,6 +1,6 @@
 from transport_co2.estimator import estimate_co2
 
-
+# Only distance
 def test_estimate_co2_bus():
     co2_estimate = estimate_co2(mode="bus", distance_in_km=10)
 
@@ -29,3 +29,34 @@ def test_estimate_co2_scooter():
     co2_estimate = estimate_co2(mode="scooter", distance_in_km=10)
 
     assert co2_estimate == 720
+
+
+# Distance and occupancy
+def test_estimate_co2_bus_with_occupancy():
+    co2_estimate = estimate_co2(mode="bus", distance_in_km=10, occupancy=10)
+
+    assert co2_estimate == 863
+
+
+def test_estimate_co2_light_rail_with_occupancy():
+    co2_estimate = estimate_co2(mode="light_rail", distance_in_km=10, occupancy=100)
+
+    assert co2_estimate == 218.4
+
+
+def test_estimate_co2_small_car_with_occupancy():
+    co2_estimate = estimate_co2(mode="small_car", distance_in_km=10, occupancy=2)
+
+    assert co2_estimate == 840.0
+
+
+def test_estimate_co2_large_car_with_occupancy():
+    co2_estimate = estimate_co2(mode="large_car", distance_in_km=10, occupancy=3)
+
+    assert co2_estimate == 733.3333333333334
+
+
+def test_estimate_co2_scooter_with_occupancy():
+    co2_estimate = estimate_co2(mode="scooter", distance_in_km=10, occupancy=2)
+
+    assert co2_estimate == 432.0
