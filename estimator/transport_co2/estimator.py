63a1bbb23da16c2ed277ac92c2e476993edd76c4
Markus Shepherd <markus.schepke@maas.global>
2020-04-16 15:50:40 +0300

    added co2_per_vehicle_km parameter, resolving #5

--- estimator/transport_co2/estimator.py
+++ estimator/transport_co2/estimator.py
@@ -7,17 +7,28 @@ from .model import Mode
 
 
 def estimate_co2(
-    mode: Union[str, Mode], distance_in_km: float, occupancy: Optional[float] = None
+    mode: Union[str, Mode],
+    distance_in_km: float,
+    *,
+    co2_per_vehicle_km: Optional[float] = None,
+    occupancy: Optional[float] = None
 ) -> float:
     """
-        Estimate CO2 usage for transport mode based on KM and optional vehicle occupancy.
+    Estimate CO2 usage for transport mode based on KM and optional vehicle occupancy.
 
-        Keyword arguments:
-            mode -- vehicle mode (limited to allowed values)
-            distance_in_km -- distance for the trip in kilometers
-            occupancy -- optional vehicle occupancy (uses average occupancy if falsey)
-        """
+    Keyword arguments:
+        mode -- vehicle mode (limited to allowed values, required)
+        distance_in_km -- distance for the trip in kilometers (required)
+        co2_per_vehicle_km -- CO2 emission in grams per vehicle km
+            (optional, uses mode average if falsey)
+        occupancy -- vehicle occupancy (optional, uses mode average if falsey)
+    """
+
     if isinstance(mode, str):
         mode = Mode[mode.upper()]
 
-    return mode.estimate_co2(distance_in_km, occupancy)
+    return mode.estimate_co2(
+        distance_in_km=distance_in_km,
+        co2_per_vehicle_km=co2_per_vehicle_km,
+        occupancy=occupancy,
+    )
