dd3d793166ef7607386b8926fe9c1901e33b1512
Brylie Christopher Oxley <brylie@amble.fi>
2020-04-23 12:10:24 +0300

    Add estimate_fuel_co2_g

--- estimator/transport_co2/estimator.py
+++ estimator/transport_co2/estimator.py
@@ -32,3 +32,27 @@ def estimate_co2(
         co2_per_vehicle_km=co2_per_vehicle_km,
         occupancy=occupancy,
     )
+
+
+def estimate_fuel_co2_g(
+    fuel: Union[str, Mode],
+    litres: float,
+    *,
+    co2_g_per_litre: Optional[float] = None
+) -> float:
+    """
+    Estimate CO2 emissions based on litre of fuel consumed.
+
+    Keyword arguments:
+        fuel -- type of fuel used (limited to allowed values, required)
+        litres -- litres of fuel consumed
+        co2_g_per_litre -- grams of CO2 released when consuming a litre of given fuel type (optional)
+    """
+
+    if isinstance(fuel, str):
+        fuel = Fuel[fuel.upper()]
+
+    return fuel.estimate_co2_g(
+        litres=litres,
+        co2_g_per_litre=co2_g_per_litre
+    )
