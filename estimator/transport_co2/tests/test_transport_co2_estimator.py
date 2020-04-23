b98ddbd01a16f5021ebab8b53d3a3eb51b000497
Brylie Christopher Oxley <brylie@amble.fi>
2020-04-23 12:34:08 +0300

    Add Fuel tests

--- estimator/transport_co2/tests/test_transport_co2_estimator.py
+++ estimator/transport_co2/tests/test_transport_co2_estimator.py
@@ -1,5 +1,5 @@
 from pytest import approx
-from transport_co2.estimator import estimate_co2
+from transport_co2.estimator import estimate_co2, estimate_fuel_co2_g
 
 
 # Only distance
@@ -41,19 +41,22 @@ def test_estimate_co2_bus_with_occupancy():
 
 
 def test_estimate_co2_light_rail_with_occupancy():
-    co2_estimate = estimate_co2(mode="light_rail", distance_in_km=10, occupancy=100)
+    co2_estimate = estimate_co2(
+        mode="light_rail", distance_in_km=10, occupancy=100)
 
     assert co2_estimate == approx(218.4)
 
 
 def test_estimate_co2_small_car_with_occupancy():
-    co2_estimate = estimate_co2(mode="small_car", distance_in_km=10, occupancy=2)
+    co2_estimate = estimate_co2(
+        mode="small_car", distance_in_km=10, occupancy=2)
 
     assert co2_estimate == approx(840.0)
 
 
 def test_estimate_co2_large_car_with_occupancy():
-    co2_estimate = estimate_co2(mode="large_car", distance_in_km=10, occupancy=3)
+    co2_estimate = estimate_co2(
+        mode="large_car", distance_in_km=10, occupancy=3)
 
     assert co2_estimate == approx(733.3333333333334)
 
@@ -62,3 +65,52 @@ def test_estimate_co2_scooter_with_occupancy():
     co2_estimate = estimate_co2(mode="scooter", distance_in_km=10, occupancy=2)
 
     assert co2_estimate == approx(432.0)
+
+
+# Fuel
+
+# Fuel type and litres
+def test_estimate_fuel_co2_g_petrol():
+    co2_estimate = estimate_fuel_co2_g(fuel="petrol", litres=1)
+
+    assert co2_estimate == approx(2_290)
+
+
+def test_estimate_fuel_co2_g_ethanol_10():
+    co2_estimate = estimate_fuel_co2_g(fuel="ethanol_10", litres=1)
+
+    assert co2_estimate == approx(2_210)
+
+
+def test_estimate_fuel_co2_g_ethanol_85():
+    co2_estimate = estimate_fuel_co2_g(fuel="ethanol_85", litres=1)
+
+    assert co2_estimate == approx(1_610)
+
+
+def test_estimate_fuel_co2_g_diesel():
+    co2_estimate = estimate_fuel_co2_g(fuel="diesel", litres=1)
+
+    assert co2_estimate == approx(2_660)
+
+
+def test_estimate_fuel_co2_g_biodiesel_5():
+    co2_estimate = estimate_fuel_co2_g(fuel="biodiesel_5", litres=1)
+
+    assert co2_estimate == approx(2_650)
+
+
+def test_estimate_fuel_co2_g_biodiesel_20():
+    co2_estimate = estimate_fuel_co2_g(fuel="biodiesel_20", litres=1)
+
+    assert co2_estimate == approx(2_620)
+
+# custom co2_g_per_litre
+
+
+def test_estimate_fuel_co2_g_biodiesel_20():
+    co2_g_per_litre = 1_000
+    co2_estimate = estimate_fuel_co2_g(
+        fuel="biodiesel_20", litres=1, co2_g_per_litre=co2_g_per_litre)
+
+    assert co2_estimate == approx(1_000)
