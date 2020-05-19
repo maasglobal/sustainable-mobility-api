288ad9af74aa6b2000621454d35b58f05051828f
Brylie Christopher Oxley <brylie@amble.fi>
2020-05-19 16:21:08 +0300

    Add remaining unit tests; simplify test cases

--- estimator/transport_co2/tests/test_transport_co2_estimator.py
+++ estimator/transport_co2/tests/test_transport_co2_estimator.py
@@ -1,116 +1,164 @@
-from pytest import approx
+from pytest import approx, fail
 from transport_co2.estimator import estimate_co2, estimate_fuel_co2_g
 
-
-# Only distance
-def test_estimate_co2_bus():
-    co2_estimate = estimate_co2(mode="bus", distance_in_km=10)
-
-    assert co2_estimate == approx(679.5275590551181)
+from transport_co2.model import Fuel, Mode
 
 
-def test_estimate_co2_light_rail():
-    co2_estimate = estimate_co2(mode="light_rail", distance_in_km=10)
+# Get list of supported transport modes and fuel types
+# by excluding Python API methods
+transport_modes = [
+    transport_mode
+    for transport_mode in Mode.__members__
+    if not transport_mode.startswith("_")
+]
 
-    assert co2_estimate == approx(140)
-
-
-def test_estimate_co2_small_car():
-    co2_estimate = estimate_co2(mode="small_car", distance_in_km=10)
-
-    assert co2_estimate == approx(1120)
-
-
-def test_estimate_co2_large_car():
-    co2_estimate = estimate_co2(mode="large_car", distance_in_km=10)
-
-    assert co2_estimate == approx(1466.6666666666667)
+fuel_types = [
+    fuel_type for fuel_type in Fuel.__members__ if not fuel_type.startswith("_")
+]
 
 
-def test_estimate_co2_scooter():
-    co2_estimate = estimate_co2(mode="scooter", distance_in_km=10)
+def test_estimate_co2():
+    """
+    Test CO2 estimates for default transport mode values
+    """
 
-    assert co2_estimate == approx(720)
+    for transport_mode in transport_modes:
+        co2_estimate = estimate_co2(mode=transport_mode.lower(), distance_in_km=10)
+
+        if transport_mode == "LIGHT_RAIL":
+            assert co2_estimate == approx(140)
+        elif transport_mode == "SMALL_CAR":
+            assert co2_estimate == approx(1_120)
+        elif transport_mode == "LARGE_CAR":
+            assert co2_estimate == approx(1_466.6666666666667)
+        elif transport_mode == "SCOOTER":
+            assert co2_estimate == approx(720)
+        elif transport_mode == "BUS":
+            assert co2_estimate == approx(679.5275590551181)
+        elif transport_mode == "WALK":
+            assert co2_estimate == approx(0)
+        elif transport_mode == "BICYCLE":
+            assert co2_estimate == approx(0)
+        elif transport_mode == "CAR":
+            assert co2_estimate == approx(1_293.3333333333333)
+        elif transport_mode == "TRAM":
+            assert co2_estimate == approx(140.0)
+        elif transport_mode == "SUBWAY":
+            assert co2_estimate == approx(62.400000000000006)
+        elif transport_mode == "RAIL":
+            assert co2_estimate == approx(140.0)
+        elif transport_mode == "FERRY":
+            assert co2_estimate == approx(816.0)
+        elif transport_mode == "CABLE_CAR":
+            assert co2_estimate == approx(140.0)
+        elif transport_mode == "GONDOLA":
+            assert co2_estimate == approx(140.0)
+        elif transport_mode == "FUNICULAR":
+            assert co2_estimate == approx(140.0)
+        elif transport_mode == "TRANSIT":
+            assert co2_estimate == approx(162.26539809714575)
+        elif transport_mode == "LEG_SWITCH":
+            assert co2_estimate == approx(0)
+        elif transport_mode == "AIRPLANE":
+            assert co2_estimate == approx(2_850.0)
+        else:
+            # Make sure we don't miss any test cases
+            # or have unexpected members
+            fail(f"No test case for { transport_mode }")
 
 
-# Distance and occupancy
-def test_estimate_co2_bus_with_occupancy():
-    co2_estimate = estimate_co2(mode="bus", distance_in_km=10, occupancy=10)
+def test_estimate_co2_with_occupancy():
+    """
+    Make sure we get correct result
+    when user provides occupancy count
+    """
 
-    assert co2_estimate == approx(863)
+    for transport_mode in transport_modes:
+        co2_estimate = estimate_co2(
+            mode=transport_mode.lower(), distance_in_km=10, occupancy=1
+        )
 
-
-def test_estimate_co2_light_rail_with_occupancy():
-    co2_estimate = estimate_co2(
-        mode="light_rail", distance_in_km=10, occupancy=100)
-
-    assert co2_estimate == approx(218.4)
-
-
-def test_estimate_co2_small_car_with_occupancy():
-    co2_estimate = estimate_co2(
-        mode="small_car", distance_in_km=10, occupancy=2)
-
-    assert co2_estimate == approx(840.0)
-
-
-def test_estimate_co2_large_car_with_occupancy():
-    co2_estimate = estimate_co2(
-        mode="large_car", distance_in_km=10, occupancy=3)
-
-    assert co2_estimate == approx(733.3333333333334)
-
-
-def test_estimate_co2_scooter_with_occupancy():
-    co2_estimate = estimate_co2(mode="scooter", distance_in_km=10, occupancy=2)
-
-    assert co2_estimate == approx(432.0)
+        if transport_mode == "LIGHT_RAIL":
+            assert co2_estimate == approx(21_840.0)
+        elif transport_mode == "SMALL_CAR":
+            assert co2_estimate == approx(1_680.0)
+        elif transport_mode == "LARGE_CAR":
+            assert co2_estimate == approx(2_200.0)
+        elif transport_mode == "SCOOTER":
+            assert co2_estimate == approx(864.0)
+        elif transport_mode == "BUS":
+            assert co2_estimate == approx(8_630.0)
+        elif transport_mode == "WALK":
+            assert co2_estimate == approx(0)
+        elif transport_mode == "BICYCLE":
+            assert co2_estimate == approx(0)
+        elif transport_mode == "CAR":
+            assert co2_estimate == approx(1_940.0)
+        elif transport_mode == "TRAM":
+            assert co2_estimate == approx(21_840.0)
+        elif transport_mode == "SUBWAY":
+            assert co2_estimate == approx(1_934.4)
+        elif transport_mode == "RAIL":
+            assert co2_estimate == approx(21_840.0)
+        elif transport_mode == "FERRY":
+            assert co2_estimate == approx(74_256.0)
+        elif transport_mode == "CABLE_CAR":
+            assert co2_estimate == approx(21_840.0)
+        elif transport_mode == "GONDOLA":
+            assert co2_estimate == approx(21_840.0)
+        elif transport_mode == "FUNICULAR":
+            assert co2_estimate == approx(21_840.0)
+        elif transport_mode == "TRANSIT":
+            assert co2_estimate == approx(10801.466666666667)
+        elif transport_mode == "LEG_SWITCH":
+            assert co2_estimate == approx(0)
+        elif transport_mode == "AIRPLANE":
+            assert co2_estimate == approx(250800.0)
+        else:
+            # Make sure we don't miss any test cases
+            # or have unexpected members
+            fail(f"No test case for { transport_mode }")
 
 
 # Fuel
+def test_estimate_fuel_co2_g():
+    """
+    Make sure we get correct result for default CO2/litre values
+    """
 
-# Fuel type and litres
-def test_estimate_fuel_co2_g_petrol():
-    co2_estimate = estimate_fuel_co2_g(fuel="petrol", litres=1)
+    for fuel_type in fuel_types:
+        co2_estimate = estimate_fuel_co2_g(fuel=fuel_type, litres=1)
 
-    assert co2_estimate == approx(2_290)
-
-
-def test_estimate_fuel_co2_g_ethanol_10():
-    co2_estimate = estimate_fuel_co2_g(fuel="ethanol_10", litres=1)
-
-    assert co2_estimate == approx(2_210)
-
-
-def test_estimate_fuel_co2_g_ethanol_85():
-    co2_estimate = estimate_fuel_co2_g(fuel="ethanol_85", litres=1)
-
-    assert co2_estimate == approx(1_610)
-
-
-def test_estimate_fuel_co2_g_diesel():
-    co2_estimate = estimate_fuel_co2_g(fuel="diesel", litres=1)
-
-    assert co2_estimate == approx(2_660)
-
-
-def test_estimate_fuel_co2_g_biodiesel_5():
-    co2_estimate = estimate_fuel_co2_g(fuel="biodiesel_5", litres=1)
-
-    assert co2_estimate == approx(2_650)
-
-
-def test_estimate_fuel_co2_g_biodiesel_20():
-    co2_estimate = estimate_fuel_co2_g(fuel="biodiesel_20", litres=1)
-
-    assert co2_estimate == approx(2_620)
-
-# custom co2_g_per_litre
+        if fuel_type == "PETROL":
+            assert co2_estimate == approx(2_290)
+        elif fuel_type == "ETHANOL_10":
+            assert co2_estimate == approx(2_210)
+        elif fuel_type == "ETHANOL_85":
+            assert co2_estimate == approx(1_610)
+        elif fuel_type == "DIESEL":
+            assert co2_estimate == approx(2_660)
+        elif fuel_type == "BIODIESEL_5":
+            assert co2_estimate == approx(2_650)
+        elif fuel_type == "BIODIESEL_20":
+            assert co2_estimate == approx(2_620)
+        else:
+            # Make sure we don't miss any test cases
+            # or have unexpected members
+            fail(f"No test case for { fuel_type }")
 
 
 def test_estimate_fuel_custom_co2_g_per_litre():
-    co2_g_per_litre = 1_000
-    co2_estimate = estimate_fuel_co2_g(
-        fuel="biodiesel_20", litres=1, co2_g_per_litre=co2_g_per_litre)
+    """
+    Make sure we get correct result
+    when user overrides CO2 g per litre
+    """
 
-    assert co2_estimate == approx(1_000)
+    co2_g_per_litre = 1_000
+
+    # Test using one litre,
+    # so value will match variable
+    co2_estimate = estimate_fuel_co2_g(
+        fuel="biodiesel_20", litres=1, co2_g_per_litre=co2_g_per_litre
+    )
+
+    assert co2_estimate == approx(co2_g_per_litre)
--- estimator/transport_co2/tests/test_transport_co2_model.py
+++ estimator/transport_co2/tests/test_transport_co2_model.py
@@ -1,116 +1,203 @@
-from pytest import approx
+from pytest import approx, fail
 from transport_co2.model import Fuel, Mode
 
+
+# Get list of supported transport modes and fuel types
+# by excluding Python API methods
+transport_modes = [
+    transport_mode
+    for transport_mode in Mode.__members__
+    if not transport_mode.startswith("_")
+]
+
+fuel_types = [
+    fuel_type for fuel_type in Fuel.__members__ if not fuel_type.startswith("_")
+]
+
+
 # Mode
+def test_allowed_transport_modes():
+    """
+    Mode enum should only have allowed transport modes
+    """
 
-# Only distance
+    allowed_transport_modes = [
+        "LIGHT_RAIL",
+        "SMALL_CAR",
+        "LARGE_CAR",
+        "SCOOTER",
+        "BUS",
+        "WALK",
+        "BICYCLE",
+        "CAR",
+        "TRAM",
+        "SUBWAY",
+        "RAIL",
+        "FERRY",
+        "CABLE_CAR",
+        "GONDOLA",
+        "FUNICULAR",
+        "TRANSIT",
+        "LEG_SWITCH",
+        "AIRPLANE",
+    ]
 
-
-def test_estimate_co2_bus():
-    co2_estimate = Mode.BUS.estimate_co2(distance_in_km=10)
-
-    assert co2_estimate == approx(679.5275590551181)
-
-
-def test_estimate_co2_light_rail():
-    co2_estimate = Mode.LIGHT_RAIL.estimate_co2(distance_in_km=10)
-
-    assert co2_estimate == approx(140)
-
-
-def test_estimate_co2_small_car():
-    co2_estimate = Mode.SMALL_CAR.estimate_co2(distance_in_km=10)
-
-    assert co2_estimate == approx(1120)
-
-
-def test_estimate_co2_large_car():
-    co2_estimate = Mode.LARGE_CAR.estimate_co2(distance_in_km=10)
-
-    assert co2_estimate == approx(1466.6666666666667)
-
-
-def test_estimate_co2_scooter():
-    co2_estimate = Mode.SCOOTER.estimate_co2(distance_in_km=10)
-
-    assert co2_estimate == approx(720)
+    assert transport_modes == allowed_transport_modes
 
 
-## Distance and occupancy
-def test_estimate_co2_bus_with_occupancy():
-    co2_estimate = Mode.BUS.estimate_co2(distance_in_km=10, occupancy=10)
+def test_mode_estimate_co2():
+    """
+    Test CO2 estimates for default transport mode values
+    """
 
-    assert co2_estimate == approx(863)
+    for transport_mode in transport_modes:
+        co2_estimate = Mode[transport_mode].estimate_co2(distance_in_km=10)
+
+        if transport_mode == "LIGHT_RAIL":
+            assert co2_estimate == approx(140)
+        elif transport_mode == "SMALL_CAR":
+            assert co2_estimate == approx(1_120)
+        elif transport_mode == "LARGE_CAR":
+            assert co2_estimate == approx(1_466.6666666666667)
+        elif transport_mode == "SCOOTER":
+            assert co2_estimate == approx(720)
+        elif transport_mode == "BUS":
+            assert co2_estimate == approx(679.5275590551181)
+        elif transport_mode == "WALK":
+            assert co2_estimate == approx(0)
+        elif transport_mode == "BICYCLE":
+            assert co2_estimate == approx(0)
+        elif transport_mode == "CAR":
+            assert co2_estimate == approx(1_293.3333333333333)
+        elif transport_mode == "TRAM":
+            assert co2_estimate == approx(140.0)
+        elif transport_mode == "SUBWAY":
+            assert co2_estimate == approx(62.400000000000006)
+        elif transport_mode == "RAIL":
+            assert co2_estimate == approx(140.0)
+        elif transport_mode == "FERRY":
+            assert co2_estimate == approx(816.0)
+        elif transport_mode == "CABLE_CAR":
+            assert co2_estimate == approx(140.0)
+        elif transport_mode == "GONDOLA":
+            assert co2_estimate == approx(140.0)
+        elif transport_mode == "FUNICULAR":
+            assert co2_estimate == approx(140.0)
+        elif transport_mode == "TRANSIT":
+            assert co2_estimate == approx(162.26539809714575)
+        elif transport_mode == "LEG_SWITCH":
+            assert co2_estimate == approx(0)
+        elif transport_mode == "AIRPLANE":
+            assert co2_estimate == approx(2_850.0)
+        else:
+            # Make sure we don't miss any test cases
+            # or have unexpected members
+            fail(f"No test case for { transport_mode }")
 
 
-def test_estimate_co2_light_rail_with_occupancy():
-    co2_estimate = Mode.LIGHT_RAIL.estimate_co2(
-        distance_in_km=10, occupancy=100)
+def test_mode_estimate_co2_with_occupancy():
+    """
+    Make sure we get correct result
+    when user provides occupancy count
+    """
 
-    assert co2_estimate == approx(218.4)
+    for transport_mode in transport_modes:
+        co2_estimate = Mode[transport_mode].estimate_co2(distance_in_km=10, occupancy=1)
 
-
-def test_estimate_co2_small_car_with_occupancy():
-    co2_estimate = Mode.SMALL_CAR.estimate_co2(distance_in_km=10, occupancy=2)
-
-    assert co2_estimate == approx(840.0)
-
-
-def test_estimate_co2_large_car_with_occupancy():
-    co2_estimate = Mode.LARGE_CAR.estimate_co2(distance_in_km=10, occupancy=3)
-
-    assert co2_estimate == approx(733.3333333333334)
-
-
-def test_estimate_co2_scooter_with_occupancy():
-    co2_estimate = Mode.SCOOTER.estimate_co2(distance_in_km=10, occupancy=2)
-
-    assert co2_estimate == approx(432.0)
+        if transport_mode == "LIGHT_RAIL":
+            assert co2_estimate == approx(21_840.0)
+        elif transport_mode == "SMALL_CAR":
+            assert co2_estimate == approx(1_680.0)
+        elif transport_mode == "LARGE_CAR":
+            assert co2_estimate == approx(2_200.0)
+        elif transport_mode == "SCOOTER":
+            assert co2_estimate == approx(864.0)
+        elif transport_mode == "BUS":
+            assert co2_estimate == approx(8_630.0)
+        elif transport_mode == "WALK":
+            assert co2_estimate == approx(0)
+        elif transport_mode == "BICYCLE":
+            assert co2_estimate == approx(0)
+        elif transport_mode == "CAR":
+            assert co2_estimate == approx(1_940.0)
+        elif transport_mode == "TRAM":
+            assert co2_estimate == approx(21_840.0)
+        elif transport_mode == "SUBWAY":
+            assert co2_estimate == approx(1_934.4)
+        elif transport_mode == "RAIL":
+            assert co2_estimate == approx(21_840.0)
+        elif transport_mode == "FERRY":
+            assert co2_estimate == approx(74_256.0)
+        elif transport_mode == "CABLE_CAR":
+            assert co2_estimate == approx(21_840.0)
+        elif transport_mode == "GONDOLA":
+            assert co2_estimate == approx(21_840.0)
+        elif transport_mode == "FUNICULAR":
+            assert co2_estimate == approx(21_840.0)
+        elif transport_mode == "TRANSIT":
+            assert co2_estimate == approx(10801.466666666667)
+        elif transport_mode == "LEG_SWITCH":
+            assert co2_estimate == approx(0)
+        elif transport_mode == "AIRPLANE":
+            assert co2_estimate == approx(250800.0)
+        else:
+            # Make sure we don't miss any test cases
+            # or have unexpected members
+            fail(f"No test case for { transport_mode }")
 
 
 # Fuel
+def test_allowed_fuel_types():
+    """
+    Mode enum should only have allowed fuel types
+    """
 
-# Only litres
-def test_fuel_co2_estimate_petrol():
-    co2_estimate_g = Fuel.PETROL.estimate_co2_g(litres=1)
+    allowed_fuel_types = [
+        "PETROL",
+        "ETHANOL_10",
+        "ETHANOL_85",
+        "DIESEL",
+        "BIODIESEL_5",
+        "BIODIESEL_20",
+    ]
 
-    assert co2_estimate_g == approx(2_290)
-
-
-def test_fuel_co2_estimate_ethanol_10():
-    co2_estimate_g = Fuel.ETHANOL_10.estimate_co2_g(litres=1)
-
-    assert co2_estimate_g == approx(2_210)
-
-
-def test_fuel_co2_estimate_ethanol_85():
-    co2_estimate_g = Fuel.ETHANOL_85.estimate_co2_g(litres=1)
-
-    assert co2_estimate_g == approx(1_610)
+    assert fuel_types == allowed_fuel_types
 
 
-def test_fuel_co2_estimate_diesel():
-    co2_estimate_g = Fuel.DIESEL.estimate_co2_g(litres=1)
+def test_estimate_fuel_co2_g():
+    """
+    Make sure we get correct result for default CO2/litre values
+    """
 
-    assert co2_estimate_g == approx(2_660)
+    for fuel_type in fuel_types:
+        co2_estimate = Fuel[fuel_type].estimate_co2_g(litres=1)
 
-
-def test_fuel_co2_estimate_biodiesel_5():
-    co2_estimate_g = Fuel.BIODIESEL_5.estimate_co2_g(litres=1)
-
-    assert co2_estimate_g == approx(2_650)
-
-
-def test_fuel_co2_estimate_biodiesel_20():
-    co2_estimate_g = Fuel.BIODIESEL_20.estimate_co2_g(litres=1)
-
-    assert co2_estimate_g == approx(2_620)
+        if fuel_type == "PETROL":
+            assert co2_estimate == approx(2_290)
+        elif fuel_type == "ETHANOL_10":
+            assert co2_estimate == approx(2_210)
+        elif fuel_type == "ETHANOL_85":
+            assert co2_estimate == approx(1_610)
+        elif fuel_type == "DIESEL":
+            assert co2_estimate == approx(2_660)
+        elif fuel_type == "BIODIESEL_5":
+            assert co2_estimate == approx(2_650)
+        elif fuel_type == "BIODIESEL_20":
+            assert co2_estimate == approx(2_620)
+        else:
+            # Make sure we don't miss any test cases
+            # or have unexpected members
+            fail(f"No test case for { fuel_type }")
 
 
 # Custom co2_g_per_litre
 def test_fuel_co2_estimate_custom_co2_g_per_litre():
     co2_g_per_litre = 1_000
-    co2_estimate_g = Fuel.BIODIESEL_20.estimate_co2_g(
-        litres=1, co2_g_per_litre=co2_g_per_litre)
 
-    assert co2_estimate_g == approx(1_000)
+    # Test using one litre,
+    # so value will match variable
+    co2_estimate_g = Fuel.BIODIESEL_20.estimate_co2_g(
+        litres=1, co2_g_per_litre=co2_g_per_litre
+    )
+
+    assert co2_estimate_g == approx(co2_g_per_litre)
